f3588e115c7c4122326478e704373c8f3f53bdd5
Luiz Carlos <luizcarlossr@gmail.com>
2025-08-20 16:25:55 -0300

    Add Regex

--- PasswordPolicy/PasswordPolicy.java
+++ PasswordPolicy/PasswordPolicy.java
@@ -1,3 +1,5 @@
+package PasswordPolicy;
+
 public class PasswordPolicy {
     public static void main(String[] args) {
         if (args.length == 0) {
---
+++ PasswordPolicy/PasswordPolicyRegex.java
@@ -0,0 +1,25 @@
+package PasswordPolicy;
+
+public class PasswordPolicyRegex {
+    public static void main(String[] args) {
+        String senha = args.length > 0 ? args[0] : "";
+
+        boolean possuiNumero = senha.matches(".*\\d.*");
+        boolean possuirLetraMaiuscula = senha.matches(".*[A-Z].*");
+        boolean possuirLetraMinuscula = senha.matches(".*[a-z].*");
+        boolean possuirCaracteresEspecial = senha.matches(".*[!@#$%^&*()\\-_=+\\[\\]{}|;:',.<>?/].*");
+
+        boolean senhaValida = possuiNumero &&
+                              possuirLetraMaiuscula &&
+                              possuirLetraMinuscula &&
+                              possuirCaracteresEspecial;
+
+        if (senhaValida) {
+            System.out.println("Sua senha foi cadastrada.");
+        } else {
+            System.out.println("Senha precisa ter entre 8 caracteres, " +
+                    "pelo menos uma letra maiúscula, " +
+                    "uma letra minúscula, um número e um caracteres especial.");
+        }
+    }
+}
