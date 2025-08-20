925a97df7f07cb7535d9ba584eb8fc4bf1b9d280
Luiz Carlos <luizcarlossr@gmail.com>
2025-08-20 11:08:22 -0300

    Adicionando senha

---
+++ PasswordPolicy.java
@@ -0,0 +1,43 @@
+public class PasswordPolicy {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Digite a senha.");
+            return;
+        }
+
+        String senha = args[0];
+
+        if (senhaValida(senha)) {
+            System.out.println("Sua senha foi cadastrada.");
+        } else {
+            System.out.println("Senha precisa ter entre 8 caracteres, " +
+                    "pelo menos uma letra maiúscula, " +
+                    "uma letra minúscula, um número e um caractere especial.");
+        }
+    }
+
+    public static boolean senhaValida(String senha) {
+        if (senha.length() < 8) {
+            return false;
+        }
+
+        boolean possuirNumero = false;
+        boolean possuirLetraMaiuscula = false;
+        boolean possuirLetraMinuscula = false;
+        boolean possuirCaracteresEspecial = false;
+
+        for (char caractere : senha.toCharArray()) {
+            if (Character.isDigit(caractere)) {
+                possuirNumero = true;
+            } else if (Character.isUpperCase(caractere)) {
+                possuirLetraMaiuscula = true;
+            } else if (Character.isLowerCase(caractere)) {
+                possuirLetraMinuscula = true;
+            } else if ("!@#$%^&*()-_=+[]{}|;:',.<>?/".indexOf(caractere) >= 0) {
+                possuirCaracteresEspecial = true;
+            }
+        }
+        return possuirNumero && possuirLetraMaiuscula &&
+                possuirLetraMinuscula && possuirCaracteresEspecial;
+    }
+}
